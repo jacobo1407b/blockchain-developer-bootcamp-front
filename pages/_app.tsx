38bdc90a1247d1d7dc788e6a8a0a5c07ef2acaed
jacobo1407b <jacobohernandezmendieta7@gmail.com>
2021-10-21 13:49:55 -0500

    dasboard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="h-screen overflow-hidden flex items-center justify-center" style={{ background: "#edf2f7" }}>
+      <div className="flex h-screen w-full bg-gray-800 " x-data="{openMenu:1}">
+      <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
 export default MyApp
